4b833fec2634f24a086013f2ccd4b087b420ea88
lkampoli <l.kampoli@spbu.ru>
2021-06-15 11:28:52 +0300

    add: paper in doc, update: README

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # STS4FlowModellium
+This code is the Fortran conversion of the Matlab version.
+
+It computes the relaxation of air (5 species) mixture behind a shock wave.
+
+Rate coefficient models:
+* FHO for vibrational transitions
+* Savelev model for Zeldovich reaction
+* Treanor-Marrone model for dissociation reactions with: U = D/6k, Park's parameters in Arrhenius law
+
+# Pre-requisites
+* linux environment
+* ifort/gfortran compilers
+* lapack/openblas/mkl libraries
+* gnuplot (optional, only for plotting)
+
+# How to use it?
+Compile in `src` by running the `compile.sh` script which will produce the executable `shprot.x`, therein, then `./shprot.x` to run.
+
+* By calling `dvode_f90(rpart_fho, ...)` will solve the full 1D problem employing the ODE solver.
+* By calling `compute_rhs(...)` will compute the RHS of the system only once. 
+  Here, some care should be used regarding the dimensions and some adjustments may be required.
+
+# Notes
+* Intel compiler is preferred for stability reasons.
+* set at least `export OMP_NUM_THREADS=1` to avoid unsafe multi-threading issues.
+* `plot.sh` not directly working since solutions files writing is commented in `main.f90` and Matlab solutions are not included.
+
--- src/compile_run_plot.sh
+++ src/compile_run_plot.sh
@@ -1,23 +1,24 @@
+#!/bin/bash
 rm shprot.x *.o *.mod *.eps
 
-ifort -c kind_module.f90 \
-            constants.f90 \
-            init_energy.f90 \
-            kdis.f90 \
-            DR_Module.f90 \
-            k_ex_savelev_st.f90 \
-            EX_Module.f90 \
-            brent_module.f90 \
-            kvt_fho.f90 \
-            VT_Module.f90 \
-            kvv_fho.f90 \
-            VV_Module.f90 \
-            rpart_fho.f90 \
-            ODE/odepack.f \
-            ODE/odepack_sub1.f \
-            ODE/odepack_sub2.f \
-            OpenMP_dvode_f90_m.f90 \
-            main.f90
+ifort -c kind_module.f90        \
+         constants.f90          \
+         init_energy.f90        \
+         kdis.f90               \
+         DR_Module.f90          \
+         k_ex_savelev_st.f90    \
+         EX_Module.f90          \
+         brent_module.f90       \
+         kvt_fho.f90            \
+         VT_Module.f90          \
+         kvv_fho.f90            \
+         VV_Module.f90          \
+         rpart_fho.f90          \
+         ODE/odepack.f          \
+         ODE/odepack_sub1.f     \
+         ODE/odepack_sub2.f     \
+         OpenMP_dvode_f90_m.f90 \
+         main.f90
 
 # compiling with gfortran is somewhow more
 # sensible to the parameter values of the
